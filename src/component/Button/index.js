5e71c6156fabc7f6fd3c2c76df14709d5e2d96c4
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-25 22:03:07 +0700

    belajar react

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -4,6 +4,6 @@ function hai(){
 export default function Button(props) {
   let {children, color} = props;
   return (
-    <button className="bg-blue-500 rounded py-2 px-4 text-gray-100" onClick="hai()">{children}</button>
+    <button className="rounded py-2 px-4 text-gray-100 bg-amber-400" onClick="hai()">{children}</button>
     )
 }
