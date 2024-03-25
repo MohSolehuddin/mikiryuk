4cf4ed5bee93f7dce89ff61ddd1383a1f98c15b2
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-25 21:50:45 +0700

    belajar react

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <body>
+    <body className="p-10 w-1 flex justify-center flex-col bg-sky-400 rounded-2xl">
       <Button color="red">klik dong</Button>
       <Button color="blue">klik dong</Button>
     </body>
