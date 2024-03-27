196693a92dea73dcbb6f22c861a5aca57095829c
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-27 21:17:55 +0700

    fix 2

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,22 @@
 import Button from '@/components/Button';
-function CardContainer(props) {
-  let {children} = props;
+
+export function CardContainer(props) {
+  const { children } = props;
   return (
-    <section id="card-item" className="bg-slate-50 rounded-2xl p-1 mx-auto
-    h-[400px] w-full px-1">
+    <section id="card-item" className="bg-slate-50 rounded-2xl p-1 mx-auto h-[400px] w-full px-1">
       {children}
     </section>
-    )
+  );
 }
 
-function CardItem(props) {
-  let {children, img, title, paragraph, } = props;
+export function CardItem(props) {
+  const { img, title, paragraph, children } = props;
   return (
-    <section  className="w-[300px] bg-slate-100 max-h-full p-1">
+    <section className="w-[300px] bg-slate-100 max-h-full p-1">
       <img className="w-full h-[200px]" src={img} alt={title} />
       <h3 className="text-2xl font-bold">{title}</h3>
       <p className="text-justify">{paragraph}</p>
       <Button>{children}</Button>
     </section>
-    )
+  );
 }
-export default {CardContainer, CardItem};
