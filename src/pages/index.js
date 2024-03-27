451989f2071f67130abf78940b7cccff3556a0a6
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-27 21:10:18 +0700

    fixing bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,52 @@
 import Button from "@/components/Button";
-import {CardCotainer, CardItem} "@/components/Card";
+import { CardContainer, CardItem } from "@/components/Card";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 const data = {
-  items[
+  items: [
     {
-      img: "https://unplash.com/computer?200px*200px",
+      img: "https://unsplash.com/computer?200px*200px",
       title: "coba",
       link: "https://github.co/ohSolehuddin/",
       paragraph: "hai ini aalaparagraff"
     },
     {
-      img: "https://unplash.com/computer?200px*200px",
+      img: "https://unsplash.com/computer?200px*200px",
       title: "coba",
       link: "https://github.co/ohSolehuddin/",
       paragraph: "hai ini aalaparagraff"
     },
     {
-      img: "https://unplash.com/computer?200px*200px",
+      img: "https://unsplash.com/computer?200px*200px",
       title: "coba",
       link: "https://github.co/ohSolehuddin/",
       paragraph: "hai ini aalaparagraff"
     },
     {
-      img: "https://unplash.com/computer?200px*200px",
+      img: "https://unsplash.com/computer?200px*200px",
       title: "coba",
       link: "https://github.co/ohSolehuddin/",
       paragraph: "hai ini aalaparagraff"
-    },
-    ]
-}
+    }
+  ]
+};
+
 export default function Home() {
   return (
     <body className="min-h-full w-full bg-slate-100 bg-no-repeat">
-      <CardCotainer>
-        {data.items.map(item)=>{
-          <CardItem img={item.img} title={item.title}
-          paragraph={item.paragraph}>klilk</CardItem>
-        }}
-      </CardCotainer>
+      <CardContainer>
+        {data.items.map((item) => (
+          <CardItem
+            key={item.title} // tambahkan key properti untuk setiap item dalam map
+            img={item.img}
+            title={item.title}
+            paragraph={item.paragraph}
+          >
+            klilk
+          </CardItem>
+        ))}
+      </CardContainer>
       <Button color="blue">klik dong</Button>
     </body>
   );
