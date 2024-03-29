a3592df30b6e1bfcfdb7e0eea0ff6896c0380aab
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-29 15:14:53 +0700

    fix

--- src/components/ScrollContainer/index.jsx
+++ src/components/ScrollContainer/index.jsx
@@ -1,6 +1,6 @@
 export default function ScrollContainer({children, }) {
   return (
-    <section id="ScrollContainer" className="w-full h-[500px]
+    <section id="ScrollContainer" className="w-full h-[600px]
     overflow-x-scroll overflow-y-hidden">
       {children}
     </section>
