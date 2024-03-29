1fc03bde8b2761bc680dbf1accc3e52dcef2e463
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-29 14:59:24 +0700

    fixing scroll fiture

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,7 @@ export function CardContainer(props) {
   const { children } = props;
   return (
     <section id="card-container" className="bg-slate-50 rounded-2xl p-1 mx-auto
-    h-[400px] w-full px-1 flex min-w-fit gap-2">
+    h-[800px] w-full px-1 flex min-w-fit gap-2">
       {children}
     </section>
   );
@@ -15,8 +15,8 @@ export function CardItem(props) {
   const { img, title, paragraph, children, link} = props;
   return (
     <section className="w-[300px] bg-slate-100 max-h-full p-1">
-      <Image className="w-full h-[200px]" src={img} alt={title} with={200}
-      height={200} />
+      <Image className="w-full h-[300px]" src={img} alt={title} with={300}
+      height={300} />
       <h3 className="text-2xl font-bold">{title}</h3>
       <p className="text-justify">{paragraph}</p>
       <Button link={link}>{children}</Button>
--- src/components/ScrollContainer/index.jsx
+++ src/components/ScrollContainer/index.jsx
@@ -1,6 +1,6 @@
 export default function ScrollContainer({children, }) {
   return (
-    <section id="ScrollContainer" className="w-full h-[400px]
+    <section id="ScrollContainer" className="w-full h-[800px]
     overflow-x-scroll overflow-y-hidden">
       {children}
     </section>
