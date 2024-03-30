57f3014026b0bd4efa18fd5b4a097a6eac09b72a
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-03-30 13:26:49 +0700

    fix: error jsx sytax

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,9 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 export default function App({ Component, pageProps }) {
   return (
-    <Navbar/>
-    <Component {...pageProps} />
+    <>
+      <Navbar/>
+      <Component {...pageProps} />
+    </>
   );
 }
