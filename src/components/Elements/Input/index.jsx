39e88819df05a6230e154f0e316f4db9ae755f29
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-04-01 20:41:21 +0700

    mengubah objek menjadi props

--- src/components/Elements/Input/index.jsx
+++ src/components/Elements/Input/index.jsx
@@ -1,12 +1,12 @@
 import Label from "./Label";
 import InputElement from "./InputElement";
 
-export default function Input({fotIt ="username", typeInput="text"}) {
+export default function Input(props) {
+  {fotIt ="username", typeInput="text"} = props;
   return (
     <>
       <Label forIt={forIt}/>
       <InputElement forIt={forIt} typeInput={typeInput}/>
-      
     </>
     )
 }
