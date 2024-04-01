4d3b2c64e2b9e4ccbddfd2e3d7d6e937a4719233
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-04-01 21:04:49 +0700

    perbaikan

--- src/components/Elements/Input/index.jsx
+++ src/components/Elements/Input/index.jsx
@@ -1,7 +1,8 @@
 import Label from "@/components/Elements/Input/Label";
 import InputElement from "@/components/Elements/Input/InputElement";
 
-export default function Input({fotIt ="username", typeInput="text"}) {
+export default function Input(props) {
+  let {fotIt ="username", typeInput="text"} = props;
   return (
     <>
       <Label forIt={forIt}/>
