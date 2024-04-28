66b7a241636067a0c20f7989b0ccd7c33f451286
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-04-28 14:08:37 +0700

    fixing

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,9 @@
 import Button from "@/components/Button";
 import Input from "@/components/Elements/Input";
 
-export default function Login({children, }) {
+export default function Login() {
   return (
-    <form className="px-1" action="/api/hallo" method="get" accept-charset="utf-8">
+    <form className="px-1" accept-charset="utf-8">
       <Input forIt="username"/>
       <Input forIt="password"/>
       <Button type="submit">Login</Button>
