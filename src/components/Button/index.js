81155e2268ead05b23b548213f812a3d30cbd6b1
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-08-02 05:10:10 +0700

    pindahan laptop

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,12 @@
 export default function Button(props) {
-  let {children = "Klik", color, link} = props;
+  let { children = "Klik", color, link } = props;
   return (
-    <a className="rounded-2xl mx-auto py-4 px-10 text-slate-100
-    bg-indigo-400 font-bold hover:bg-indigo-500" href={link} about="_blank">{children}</a>
-    )
+    <a
+      className="rounded-2xl mx-auto py-4 px-10 text-slate-100
+    bg-indigo-400 font-bold hover:bg-indigo-500"
+      href={link}
+      about="_blank">
+      {children}
+    </a>
+  );
 }
